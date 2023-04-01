b8294f892dbee988cdf6a018186403803803586f
Sara Mali <mali.sara7@gmail.com>
2023-04-01 13:17:52 +0200

    move user

--- space-invaders.js
+++ space-invaders.js
@@ -7,12 +7,14 @@ const gameGrid = document.getElementById('grid');
 const scoreGrid = document.getElementById('score');
 
 const NUMBER_BLOCKS_ROW = 20;
-
+const NUMBER_ALL_SQUARES = 400;
 
 let allDivsArray = []; //IDs
 let aliensArray = []; //IDs
+let removedAliens = [];
 let level = 0;
 let score = 0;
+let currentUserLocation = 389;
 
 
 btnChooseLevel.forEach(button => {
@@ -31,6 +33,8 @@ function startNewGame() {
     scoreGrid.innerHTML = 'Score: ' + score;
     createBaseDivs();
     createAlienArray();
+    allDivsArray[currentUserLocation].classList.add('user'); //Get user
+
 }
 
 function createBaseDivs() {
@@ -58,7 +62,92 @@ function createAlienArray() {
 }
 
 
+document.addEventListener('keydown', moveUser);
+
+
+function moveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentUserLocation > NUMBER_ALL_SQUARES - NUMBER_BLOCKS_ROW) {
+                deleteUser();
+                currentUserLocation -= 1;
+                reDrawUser(currentUserLocation);
+            }
+            break;
+
+        case 'ArrowRight':
+            if (currentUserLocation < NUMBER_ALL_SQUARES - 1) {
+                deleteUser();
+                currentUserLocation += 1;
+                reDrawUser(currentUserLocation);
+            }
+            break;
+
+        case 'ArrowUp':
+            shoot();
+            break;
+    }
+}
+
+function deleteUser() {
+    allDivsArray.forEach(div => {
+        div.classList.remove('user');
+    });
+}
+
+function reDrawUser(userLocation) {
+    allDivsArray[userLocation].classList.add('user');
+}
+
+
+function shoot() {
+    let bulletLocation = currentUserLocation - NUMBER_BLOCKS_ROW;
+    redrawBullet(bulletLocation);
+    let shootingInterval = setInterval(moveBullet, 20);
+
+    function moveBullet() {
+
+        removeBullets();
+        if (bulletLocation - NUMBER_BLOCKS_ROW >= 0) {
+            bulletLocation -= NUMBER_BLOCKS_ROW;
+            redrawBullet(bulletLocation);
+            if (allDivsArray[bulletLocation].classList.contains('alien')) {
+                score++;
+                scoreGrid.innerHTML = 'Score: ' + score;
+                removedAliens.push(aliensArray.indexOf(Number(allDivsArray[bulletLocation].id)));
+                allDivsArray[bulletLocation].classList.remove('alien');
+                clearInterval(shootingInterval);
+                removeBullets();
+
+                if (aliensArray.length === removedAliens.length) {
+                    youWin();
+                }
+            }
+        }
+    }
+}
+
+
+function redrawBullet(bulletLocation) {
+    const bullet = allDivsArray[bulletLocation];
+    bullet.classList.add('bullet');
+}
+
+
+function removeBullets() {
+    allDivsArray.forEach(div => {
+        div.classList.remove('bullet');
+    });
+};
+
+
 function resetValues() {
     allDivsArray = [];
     aliensArray = [];
+    removedAliens = [];
+}
+
+function youWin() {
+    console.log('yaay');
+    //TODO
 }
